449f493b676070481f2a45d2d3f497db75f579a3
DulanjanaLakshan <dulanjana20013@gmail.com>
2023-11-14 08:05:00 +0530

    update

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidenavbarComponent } from '../../components/sidenavbar/sidenavbar.component';
 import { TopnavbarComponent } from '../../components/topnavbar/topnavbar.component';
@@ -44,28 +44,31 @@ interface Data {
 export class ProfileComponent implements OnInit {
 
   public getData: Data[] = [];
+
   public id: Number = 0;
   public name: String = '';
   public contact: String = '';
   public email: String = '';
   public username: String = '';
   public password: String = '';
+
   selectedRowData: any;
-  
+
 
   ngOnInit() {
     this.getUser();
   }
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService, private cdr: ChangeDetectorRef) { }
   public async getUser() {
     try {
       const response = await axios.get("https://localhost:8080/api/User");
       this.getData = response.data;
-      this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Message Content' });
+      this.cdr.detectChanges();
     } catch (error) {
     }
   }
-  public async save() {
+
+  public save() {
     let req = {
       id: this.id,
       name: this.name,
@@ -76,16 +79,46 @@ export class ProfileComponent implements OnInit {
     }
 
     try {
-      const response = await axios.post("", req);
-      this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Message Content' });
+      axios.post("https://localhost:8080/api/User", req);
       this.getUser();
+      this.clear();
     } catch (error) { }
 
   }
-  public async update() {
-    console.log("work");
-    
-    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Message Content' });
+
+  public update() {
+    let req = {
+      id: this.id,
+      name: this.name,
+      contact: this.contact,
+      email: this.email,
+      username: this.username,
+      password: this.password,
+    }
+
+    try {
+      axios.put("https://localhost:8080/api/User", req);
+      this.getUser();
+      this.clear();
+    } catch (error) { }
+  }
+
+  public async delete() {
+    let req = {
+      id: this.id,
+      name: this.name,
+      contact: this.contact,
+      email: this.email,
+      username: this.username,
+      password: this.password,
+    }
+
+    try {
+      const response = await axios.delete(`https://localhost:8080/api/User/${req.id}`);
+      this.getData = response.data;
+      this.cdr.detectChanges();
+      this.clear();
+    } catch (error) { }
   }
 
   onRowSelect(dataItem: any): void {
@@ -96,6 +129,14 @@ export class ProfileComponent implements OnInit {
     this.contact = dataItem.contact;
     this.email = dataItem.email;
     this.username = dataItem.username;
+    this.password = '';
+  }
+  private clear(){
+    this.id = 0;
+    this.name = '';
+    this.contact ='';
+    this.email = '';
+    this.username = '';
     this.password = '';
   }
 }
--- src/app/pages/shopping-complex/shopping-complex.component.ts
+++ src/app/pages/shopping-complex/shopping-complex.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidenavbarComponent } from '../../components/sidenavbar/sidenavbar.component';
 import { TopnavbarComponent } from '../../components/topnavbar/topnavbar.component';
@@ -7,23 +7,70 @@ import { InputTextModule } from 'primeng/inputtext';
 import { CheckboxModule } from 'primeng/checkbox';
 import { RadioButtonModule } from 'primeng/radiobutton';
 import { ButtonModule } from 'primeng/button';
+import axios from 'axios';
+import { FormsModule } from '@angular/forms';
+
+interface Data {
+  id: Number;
+  name: String;
+  Location: String;
+  TotalStores: String;
+}
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [
-    CommonModule, 
-    SidenavbarComponent, 
-    TopnavbarComponent, 
+    CommonModule,
+    SidenavbarComponent,
+    TopnavbarComponent,
     TableModule,
     InputTextModule,
     CheckboxModule,
     RadioButtonModule,
     ButtonModule,
+    FormsModule
   ],
   templateUrl: './shopping-complex.component.html',
   styleUrl: './shopping-complex.component.css'
 })
-export class ShoppingComplexComponent {
+export class ShoppingComplexComponent implements OnInit {
+  public getData: Data[] = [];
+  public id: Number = 0;
+  public name: String = '';
+  public Location: String = '';
+  public TotalStores: String = '';
+
+
+
+  ngOnInit() {
+    this.getShoppingComplex();
+  }
+
+  public async getShoppingComplex() {
+    try {
+      const response = await axios.get('https://localhost:8080/api/ShoppingComplex');
+      this.getData = response.data;
+    } catch (error) { }
+  }
+
+  save() {
+    let req = {
+      id: this.id,
+      name: this.name,
+      Location: this.Location,
+      TotalStores: this.TotalStores,
+    }
+    try {
+      axios.post("https://localhost:8080/api/ShoppingComplex", req);
+      this.getShoppingComplex();
+    } catch (error) { }
+  }
+  delete() {
+    throw new Error('Method not implemented.');
+  }
+  update() {
+    throw new Error('Method not implemented.');
+  }
 
 }
